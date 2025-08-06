d4827d969ffd375d8e466e80b4bb182f5b1f3e08
Israel <iortega@dgp.unam.mx>
2025-08-06 12:36:15 -0600

    fix: guardado en bitacora

--- src/main/java/mx/unam/sa/ponentes/config/CustomLogoutSuccessHandler.java
+++ src/main/java/mx/unam/sa/ponentes/config/CustomLogoutSuccessHandler.java
@@ -1,22 +1,40 @@
 package mx.unam.sa.ponentes.config;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import mx.unam.sa.ponentes.service.RegisterService;
 
 @Component
 @Slf4j
 public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
+    private final RegisterService registerService;
+
+    public CustomLogoutSuccessHandler(RegisterService registerService) {
+        this.registerService = registerService;
+    }
+
     @Override
     public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
             throws IOException, ServletException {
+
         if (authentication != null) {
-            log.info("Logout success for user: {}", authentication.getName());
+            String username = authentication.getName();
+            String localAddress = request.getLocalAddr();
+            try {
+                registerService.saveRegister(username, localAddress, "salida");
+            } catch (Exception e) {
+                System.out.println("Error saving register of exit: " + e.getMessage() + " for user: " + username + " from IP: "
+                        + localAddress);
+            }
+
         } else {
             log.info("Logout success for anonymous user");
         }
--- src/main/java/mx/unam/sa/ponentes/config/CustomOauth2SuccessHandler.java
+++ src/main/java/mx/unam/sa/ponentes/config/CustomOauth2SuccessHandler.java
@@ -1,22 +1,38 @@
 package mx.unam.sa.ponentes.config;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.extern.slf4j.Slf4j;
+import java.io.IOException;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import mx.unam.sa.ponentes.service.RegisterService;
 
 @Component
 @Slf4j
 public class CustomOauth2SuccessHandler implements AuthenticationSuccessHandler {
 
+    RegisterService registerService;
+
+    CustomOauth2SuccessHandler (RegisterService registerService) {
+        this.registerService = registerService;
+    }
+    
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-        log.info("Authentication success for user: {}", authentication.getName());
+       
+        String username = authentication.getName();
+        String localAddress = request.getLocalAddr();
+        try {
+            registerService.saveRegister(username, localAddress, "ingreso");    
+        } catch (Exception e) {
+            System.out.println("Error saving register: " + e.getMessage() + " for user: " + username + " from IP: " + localAddress);
+        }
+
         try {
             response.sendRedirect("/");
             log.info("Redirecting to /");
